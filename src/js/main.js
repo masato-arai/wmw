5ed2e12bad8258a522bf556ad8de8a51ce073020
Masato Seki <masatoseki@Masatos-MacBook-Air.local>
2018-09-15 23:00:35 +0100

    WIP carousel

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,7 @@
 import FastClick from 'fastclick';
 
+import carousel from '../../templates/components/carousel';
+
 FastClick.attach(document.body);
+
+carousel();
---
+++ templates/components/carousel/index.js
@@ -0,0 +1,22 @@
+import $ from 'jquery';
+import 'slick-carousel';
+
+function carouselInit() {
+  $('.js-carousel-main')
+    .slick({
+      cssEase: 'ease-in-out',
+      draggable: false,
+      dots: true,
+      slidesToShow: 1,
+      lazyLoad: 'progressive',
+      speed: 300,
+      prevArrow: $('.js-carousel-ui-prev'),
+      nextArrow: $('.js-carousel-ui-next'),
+    });
+}
+
+const carousel = () => {
+  carouselInit();
+};
+
+export default carousel;
