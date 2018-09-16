6ed79da50f1f58c261e4cf7c5b254c30ba4a761b
Masato Seki <masatoseki@Masatos-MacBook-Air.local>
2018-09-16 10:58:38 +0100

    Mega menu style

--- templates/components/header-menu/components/header-nav/mega-menu.js
+++ templates/components/header-menu/components/header-nav/mega-menu.js
@@ -1,35 +1,32 @@
 import $ from 'jquery';
 import { hideCart } from '../cart';
 
-// const showChild = event => {
-//   $(event.currentTarget)
-//     .parent()
-//     .find('.js-unit-share__child')
-//     .toggleClass('unit-share__child--show-child');
-// };
-
 function showMenu() {
   hideCart();
 
   $(this)
     .parent()
-    .find('.js-mega-menu')
+    .find('.js-mega-menu-bridge')
     .addClass('show');
+
+  const menuType = $(this).attr('menu-type');
+  $(`.js-mega-menu[menu-type='${menuType}']`).addClass('show');
 }
 
 export function hideMenu() {
   $(this)
     .parent()
-    .find('.js-mega-menu')
+    .find('.js-mega-menu-bridge')
     .removeClass('show');
+
+  const menuType = $(this).attr('menu-type');
+  $(`.js-mega-menu[menu-type='${menuType}']`).removeClass('show');
 }
 
 const megaMenu = () => {
-  $('.js-mega-menu-opener, .js-mega-menu')
+  $('.js-mega-menu-opener, .js-mega-menu-bridge, .js-mega-menu')
     .mouseout(hideMenu)
     .mouseover(showMenu);
-
-  // $('.js-mega-menu-opener').on('click', showChild);
 };
 
 export default megaMenu;
