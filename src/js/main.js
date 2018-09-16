fa1000057997d9195f3f0a7f0c60c75e37a5b80f
Masato Seki <masatoseki@Masatos-Air.lan>
2018-09-16 22:46:58 +0100

    Installed retinajs

--- src/js/main.js
+++ src/js/main.js
@@ -1,10 +1,14 @@
 import FastClick from 'fastclick';
 
+import retinaImage from './modules/retina-image';
+
 import carousel from '../../templates/components/carousel';
 import cart from '../../templates/components/header-menu/components/cart';
 import megaMenu from '../../templates/components/header-menu/components/header-nav/mega-menu';
 
 FastClick.attach(document.body);
+
+retinaImage();
 
 carousel();
 cart();
